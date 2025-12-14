775320101d102fd4b741cb90c5d046eee72ccd1b
Jere655 <jeremie.lavalliere@hotmail.com>
2025-12-14 14:30:37 -0500

    Add SceneExecutor to create game objects from JSON

--- Runtime/Scripts/SceneExecutor.cs
+++ Runtime/Scripts/SceneExecutor.cs
@@ -1,1 +1,25 @@
+using UnityEngine;
+using System;
 
+[Serializable]
+public class AICommand
+{
+    public string shape;
+    public float[] position;
+    public float[] scale;
+}
+
+public class SceneExecutor : MonoBehaviour
+{
+    public void Execute(string json)
+    {
+        AICommand cmd = JsonUtility.FromJson<AICommand>(json);
+
+        GameObject obj = GameObject.CreatePrimitive(
+            cmd.shape == "sphere" ? PrimitiveType.Sphere : PrimitiveType.Cube
+        );
+
+        obj.transform.position = new Vector3(cmd.position[0], cmd.position[1], cmd.position[2]);
+        obj.transform.localScale = new Vector3(cmd.scale[0], cmd.scale[1], cmd.scale[2]);
+    }
+}
